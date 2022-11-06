8596c5347d8444cf389fe61929a13496f86f76d7
Kaustubh Agrawal <kaustubhagrawal8838@gmail.com>
2022-11-06 20:41:23 +0530

    windows exe fixed

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 echo "Zipping files..."
-zip hangman.zip fonts/* src/* main.lua README.md
+zip hangman.zip fonts/* src/* main.lua conf.lua README.md
 
 echo "Renaming..."
 mv hangman.zip hangman.love
@@ -9,11 +9,18 @@ mv hangman.love ./builds
 
 cd builds
 
-FILE=./love.exe
+DIR=love
 
-if [ -f "$FILE" ]; then
+if [ -d "$DIR" ]; then
   echo "Creating windows builds..."
-  cat ./love.exe hangman.love > hangman-windows.exe
+  mkdir temp
+  cp love/* temp
+  cat temp/love.exe hangman.love > temp/hangman.exe
+  cd temp
+  zip hangman-windows.zip *
+  cp hangman-windows.zip ../
+  cd ..
+  rm -r temp
   echo "Windows build Complete!"
 else 
   echo "If you donot have a love.exe file in your builds directory. Please download the executable from https://www.love2d.org/"
